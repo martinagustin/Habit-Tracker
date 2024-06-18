99febff0d1e6573ba24d0897bd48ca33075602c6
martinagustin <g4rciamartin@gmail.com>
2024-06-18 08:39:10 +0200

    se soluciono la incorporacion de Sqlite y se creo el menu

--- Habit-Tracker/Program.cs
+++ Habit-Tracker/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Generic;
+using Microsoft.Data.Sqlite;
+
+string connectionString=@"Data source=habit-tracker.db";
+    using (var connection = new SqliteConnection(connectionString))
+    {
+        connection.Open();
+        var tableCmd=connection.CreateCommand();
+
+        tableCmd.CommandText = 
+        @"CREATE TABLE IF NOT EXISTS drinking_water (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            Date TEXT,
+            Quantity INTEGER
+        )";
+
+        tableCmd.ExecuteNonQuery();
+    }
+
+static void GetUserInput()
+{
+    Console.Clear();
+    bool closeapp= false;
+    while(closeapp == false ){
+        System.Console.WriteLine("\n\n MAIN MENU");
+        System.Console.WriteLine("\nWhat would you like to do?");
+        System.Console.WriteLine("\nType '0' to Close Application.");
+        System.Console.WriteLine("\nType '1' to View All Records.");
+        System.Console.WriteLine("\nType '2' to Insert Records.");
+        System.Console.WriteLine("\nType '3' to Delete Records.");
+        System.Console.WriteLine("\nType '4' to Update Records.");
+    }
+}
